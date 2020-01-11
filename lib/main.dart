cbd4e086844f7c6eb6f100631a9c064465aa94ca
Jan Salvador Sebastian <sebastian@mclinica.com>
2020-01-11 22:02:28 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _hexInput = "";
+  String _rgbInput = "";
+  double _screenWidth;
+
+  void _incrementCounter() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _screenWidth = MediaQuery.of(context).size.width / 8;
+    return Scaffold(
+      // backgroundColor: Color(0xff000000),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: _screenWidth, right: _screenWidth),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    style: TextStyle(color: Color(0xffffffff)),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Hex',
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
